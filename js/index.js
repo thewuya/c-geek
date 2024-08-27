4e399f3581a110c881008a1638e9732d4f4aca20
thewuya <josue2024bit@outlook.com>
2024-08-27 23:41:26 +0800

    ad

--- js/index.js
+++ js/index.js
@@ -45,23 +45,59 @@ request.onsuccess = function(event) {
 
 
   if(window.location.pathname.endsWith('quiz_1.html')) {
-    const questions = [
-      { 
-        question: "在C语言中,正确引入标准输入输出库的语法是什么？",
-        options: ["stdio.h", "sstdii.h", "#sstdio.h", "Mdsadsad"],
-        answer: "stdio.h"
-      },
-      { 
-        question: "¿Qué se come en Nicaragua?",
-        options: ["Arroz", "Papa", "Maíz", "Frijoles"],
-        answer: "Arroz"
-      },
-      { 
-        question: "¿Qué se bebe en Nicaragua?",
-        options: ["Arroz", "Papa", "Maíz", "Agua"],
-        answer: "Agua"
-      }
-    ];
+    
+const questions = [
+  { 
+    question: "下列for循环的次数为",
+    options: ["5", "6", "1", "无限"],
+    answer: "6"
+  },
+  { 
+    question: "下述关于循环体的描述中，（C）是错误的。",
+    options: ["循环体中可以出现break语句和continue语句", "循环体中还可以出现循环语句", "循环体中不能出现goto语句", "循环体中可以出现开关语句"],
+    answer: "循环体中不能出现goto语句"
+  },
+  { 
+    question: "有以下程序段：int n=0,p; do { scanf(”%d”, &p); n++;} while(p!=12345&&n<3); 此处do-while循环的结束条件是（D）。",
+    options: ["p的值不等于12345并且n的值小于3", "p的值等于12345并且n的值大于等于3", "p的值不等于12345或者n的值小于3", "p的值等于12345或者n的值大于等于3"],
+    answer: "p的值等于12345或者n的值大于等于3"
+  },
+  { 
+    question: "以下程序中，while循环的循环次数是（D）",
+    options: ["1", "10", "6", "死循环，不能确定次数"],
+    answer: "死循环，不能确定次数"
+  },
+  { 
+    question: "下列while循环的执行次数是（A）",
+    options: ["0", "1", "5", "死循环"],
+    answer: "0"
+  },
+  { 
+    question: "在下列选项中，没有构成死循环的程序段是（D）",
+    options: ["int i=100; while (1) { i=i%100+1; if (i>100) break; }", "for( ; ; );", "int k=1000; do {++k;} while (k>=1000);", "int s=36; while (s) --s;"],
+    answer: "int s=36; while (s) --s;"
+  },
+  { 
+    question: "以下程序的输出结果是",
+    options: ["101", "6", "4", "3"],
+    answer: "3"
+  },
+  { 
+    question: "以下程序的功能是:从键盘上输入若干个学生的成绩, 统计并输出最高成绩和最低成绩, 当输入负数时结束输入。请填空。（D）",
+    options: ["x<=0; x>amin; x<=amin", "x>0; x>amin; x<=amin", "x>0; x>amin; x<amin", "x>=0; x>amin; x<amin"],
+    answer: "x>=0; x>amin; x<amin"
+  },
+  { 
+    question: "函数pi的功能是根据以下近似公式求π值：请填空，完成求π的功能。（C）",
+    options: ["1/i*i", "1.0/i*i", "1.0/(i*i)", "1.0/(n*n)"],
+    answer: "1.0/(i*i)"
+  },
+  { 
+    question: "设有以下程序：程序运行后，如果从键盘上输入1298，则输出结果为（B）",
+    options: ["892", "8921", "89", "921"],
+    answer: "8921"
+  }
+];
     quizLoader('quiz_1', questions);
   }
 
@@ -119,6 +155,8 @@ function handleSignInForm(event) {
     if (request.result) {
       alert('客户名已经被选了')
     } else {
+
+      
       const transaction = db.transaction(['users'], 'readwrite');
       const objectStore = transaction.objectStore('users');
 
@@ -145,9 +183,12 @@ function handleSignInForm(event) {
 
       addRequest.onsuccess = function() {
         console.log('成功了');
-        sessionStorage.setItem('log_username', username);
-        sessionStorage.setItem('log_password', password);
-        window.location.href = 'log_in.html'
+        document.getElementById('username').value = username;
+        document.getElementById('password').value = password;
+       
+        const flipCardInner = document.querySelector('.flip-card__inner');
+        flipCardInner.classList.remove('flipped');
+        
       };
 
       addRequest.onerror = function () {
@@ -225,7 +266,7 @@ function loadProfile (){
 
   const username = document.getElementById('username');
   const password = document.getElementById('password');
-  const gender = document.getElementById('gender');
+  
   const experience = document.getElementById('experience');
   const level = document.getElementById('level');
   const lessonCompleted = document.getElementById('lessonCompleted');
@@ -240,7 +281,6 @@ function loadProfile (){
     const user = request.result;
     username.innerHTML = user.username;
     password.innerHTML = user.password;
-    gender.innerHTML = user.gender;
     experience.innerHTML = user.experience;
     level.innerHTML = user.level;
     lessonCompleted.innerHTML = user.lessonCompleted;
@@ -516,14 +556,14 @@ function quizLoader(quiz_name, questions) {
         const updateRequest = objectStore.put(user);
 
         updateRequest.onsuccess = function() {
-          alert(`¡Has respondido correctamente a ${score} de ${totalQuestions} preguntas! Has ganado ${score * 10} puntos de experiencia.`);
+          alert(`你已经正确回答了 ${score} 道题，共 ${totalQuestions} 道题！你获得了 ${score * 10} 点经验值。`);
         };
 
         updateRequest.onerror = function() {
           console.error('Actualización fallida: ', updateRequest.error);
         };
       } else {
-        alert('Ya has completado este cuestionario.');
+        alert('你做过啊');
       }
     };
 
